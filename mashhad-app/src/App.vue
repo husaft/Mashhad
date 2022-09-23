a5288d94b2e2418517162cf4c4113da121c5290d
husaft <hubertussack@outlook.com>
2022-09-23 22:02:28 +0200

    Inserting some space and tabs

--- mashhad-app/src/App.vue
+++ mashhad-app/src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
-    <h1>Transliteration</h1>
+    <b-card>
+        <b-tabs content-class="mt-3">
+            <b-tab title="Transliteration" active>
+                <label for="ta_roman"><h2>Roman:</h2></label>
+                <b-form-textarea id="ta_roman" 
+                    rows="5" max-rows="6" 
+                    v-model="romanTxt" 
+                    @input="onRoman" 
+                    placeholder="Write some text"></b-form-textarea>
 
-    <label for="ta_roman"><h2>Roman:</h2></label>
-    <textarea id="ta_roman" 
-        rows="5" cols="50" 
-        v-model="romanTxt" 
-        @input="onRoman" 
-        placeholder="Write some text">1234567</textarea>
-
-    <label for="ta_farsi"><h2>Farsi:</h2></label>
-    <textarea id="ta_farsi" 
-        rows="5" cols="42" 
-        v-model="farsiTxt" 
-        @input="onFarsi" 
-        placeholder="Write some text"></textarea>
+                <label for="ta_farsi"><h2>Farsi:</h2></label>
+                <b-form-textarea id="ta_farsi" 
+                    rows="5" max-rows="6" 
+                    v-model="farsiTxt" 
+                    @input="onFarsi" 
+                    placeholder="Write some text"></b-form-textarea>
+            </b-tab>
+        </b-tabs>
+    </b-card>
   </div>
 </template>
 
